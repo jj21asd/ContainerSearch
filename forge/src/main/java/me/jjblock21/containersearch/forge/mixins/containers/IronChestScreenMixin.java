2e050cda80603ccd9754fc5548f3ec8b63ff4c6c
jj21 <jjblock21@gmail.com>
2025-10-24 23:25:42 +0200

    Add support for Iron Chests containers

---
+++ forge/src/main/java/me/jjblock21/containersearch/forge/mixins/containers/IronChestScreenMixin.java
@@ -0,0 +1,17 @@
+package me.jjblock21.containersearch.forge.mixins.containers;
+
+import com.progwml6.ironchest.client.screen.IronChestScreen;
+import me.jjblock21.containersearch.core.SearchProvider;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.SoftOverride;
+
+@Pseudo
+@Mixin(IronChestScreen.class)
+public class IronChestScreenMixin {
+    @SoftOverride
+    protected SearchProvider container_search$createSearchProvider() {
+        IronChestScreen screen = (IronChestScreen)(Object)this;
+        return new SearchProvider(screen, screen.getScreenHandler().getContainer());
+    }
+}
